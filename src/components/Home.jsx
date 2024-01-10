dce8ee2e205e0a760041f3b53dc8250432f24aa0
LanceEnr <lancemikaelo.enriquez@gmail.com>
2024-01-10 22:27:48 +0800

    push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-yellow-600">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Lance Enriquez
+          Lance Enriquez,
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           Aspiring Web Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          Welcome! I’m a 4th year BS Information Technology student at the
-          University of Santo Tomas, currently exploring the world of web
-          development and seeking opportunities to learn and grow.
+          I’m a 4th year BS Information Technology student at the University of
+          Santo Tomas, currently exploring the world of web development and
+          seeking opportunities to learn and grow.
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600">
